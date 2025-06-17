009c80f50a17cfb0c31d5f9397e360ffef13c1e5
JustinBenito <justinbenito1974@gmail.com>
2025-06-17 13:09:34 +0530

    push pannitaen

--- mediapipe-gpt.py
+++ mediapipe-gpt.py
@@ -1,13 +1,14 @@
 import gltf
 from gltf._loader import GltfLoader
 from panda3d.core import LoaderFileTypeRegistry
-import time
-
+from panda3d.core import ClockObject
+import math
 LoaderFileTypeRegistry.getGlobalPtr().register_deferred_type(GltfLoader)
 from direct.actor.Actor import Actor
 from direct.showbase.ShowBase import ShowBase
 from panda3d.core import AmbientLight, DirectionalLight, Vec4
 import simplepbr  # optional, but recommended
+globalClock = ClockObject.getGlobalClock()
 
 class TestApp(ShowBase):
     
@@ -28,27 +29,33 @@ class TestApp(ShowBase):
         # model.reparentTo(self.render)
         model = Actor("Demon3_Low_Poly.glb")
         model.reparentTo(self.render)
+        model.setRenderModeWireframe()
         joint_names = [j.getName() for j in model.getJoints()]
         print(joint_names)
 
-        # for name in joint_names:
-        #     joint_np = model.exposeJoint(None, 'modelRoot', name)
-        #     # Create a small sphere to represent the joint
-        #     marker = self.loader.loadModel("models/misc/sphere")  # Panda3D includes this by default
-        #     marker.setScale(0.1)
-        #     marker.setColor(1, 1, 1, 1)  # Red
-        #     marker.reparentTo(joint_np)
+        for name in joint_names:
+            joint_np = model.exposeJoint(None, 'modelRoot', name)
+            # Create a small sphere to represent the joint
+            marker = self.loader.loadModel("models/misc/sphere")  # Panda3D includes this by default
+            marker.setScale(0.05)
+            marker.setColor(1, 0.2, 0.5, 1)  # Red
+            marker.reparentTo(joint_np)
 
         model.setScale(0.5)
         model.setPos(0, 2, 0)
         model.setHpr(0,0,0)
         model.getJoints()
 
-        joint_np = model.controlJoint(None, 'modelRoot', 'Head')
+        self.joint_np = model.controlJoint(None, 'modelRoot', 'Head')
 
+        self.time = 0
+        self.taskMgr.add(self.animate_joint, "AnimateJoint")
 
-        joint_np.setHpr(45,0,0)
-
+    def animate_joint(self, task):
+        self.time += globalClock.getDt()
+        angle = math.sin(self.time * 2) * 30  # oscillate between -30 to +30 degrees
+        self.joint_np.setHpr(angle, 0, 0)
+        return task.cont
 
 
 app = TestApp()
